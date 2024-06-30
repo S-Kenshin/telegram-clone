3df3a77764ffacb54798d648df49aca7a2e8603f
S-Kenshin <ps0139hk@gmail.com>
2024-06-30 16:50:53 +0900

    ini commit

---
+++ src/app/(home)/(tabs)/_layout.tsx
@@ -0,0 +1,5 @@
+import { Tabs, Stack } from "expo-router";
+
+export default function TabsNavigator() {
+  return <Tabs />;
+}
---
+++ src/app/_lauout.tsx
@@ -0,0 +1,7 @@
+// Define grobal providers
+
+import { Slot, Stack } from "expo-router";
+
+export default function RootLayout() {
+  return <Stack />;
+}
